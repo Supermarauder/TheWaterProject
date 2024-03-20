87dd14156c6d7894947fda4ed8d569bac69cd5f2
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-03-20 09:48:27 -0600

    Add project files.

---
+++ TheWaterProject/Models/EFWaterRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace TheWaterProject.Models
+{
+    public class EFWaterRepository : IWaterRepository
+    {
+        private WaterProjectContext _context;
+        public EFWaterRepository(WaterProjectContext temp) {
+
+            _context = temp;
+        } //Constructor
+
+
+        public IQueryable<Project> Projects => _context.projects;
+    }
+}
